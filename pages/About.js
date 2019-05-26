5b091d9a6ae6261d8f4b66c0e1ad00d2b2786421
Imran Sayed <imranhsayed@gmail.com>
2019-05-26 14:30:11 +0530

    Add title to the links

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,14 @@
+import Link from "next/link";
+
 export default function About() {
 	return (
 		<div>
+			<Link href="/" >
+				<a>Home Page</a>
+			</Link>
+			<Link href="/about" >
+				<a>About Page</a>
+			</Link>
 			<p>This is the about page</p>
 		</div>
 	)
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 const Index = () => (
 	<div>
+		{/*Add Nav Links*/}
+		<Link href="/" >
+			<a title="home page">Home Page</a>
+		</Link>
+		<Link href="/about" >
+			<a title="about page">About Page</a>
+		</Link>
+
+
 		<p>Hello Next.js</p>
 	</div>
 );
 
-export default Index
+export default Index;
