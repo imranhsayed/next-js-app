460b06f11f790c3a8a5c1519f2604a43cb926580
Imran Sayed <imranhsayed@gmail.com>
2019-05-28 00:02:13 +0530

    Add server side clean url

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-const express = require( 'express' );
+const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next( dev );
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare()
-	.then( () => {
+app
+	.prepare()
+	.then(() => {
 		const server = express();
 
-		server.get( '*', ( req, res ) => {
-			return handle( req, res );
-		} );
+		server.get('/p/:id', (req, res) => {
+			const actualPage = '/post';
+			const queryParams = { title: req.params.id };
+			app.render(req, res, actualPage, queryParams);
+		});
 
-		server.listen( 3000, err => {
-			if ( err ) throw err;
-			console.warn( 'Listening on http://localhost:3000' );
-		} );
-	} )
-	.catch( ex => {
-		console.warn( ex.stack );
-		process.exit( 1 );
-	} );
+		server.get('*', (req, res) => {
+			return handle(req, res)
+		});
 
+		server.listen(3000, err => {
+			if (err) throw err;
+			console.log('> Ready on http://localhost:3000')
+		})
+	})
+	.catch(ex => {
+		console.error(ex.stack);
+		process.exit(1)
+	});
