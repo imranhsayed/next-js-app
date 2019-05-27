bc590e2c1f4a49c3f58f4fddd425fc4211c434d8
Imran Sayed <imranhsayed@gmail.com>
2019-05-28 00:39:54 +0530

    Depoloyed to now

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
-import Layout from "../components/Layout";
-import Link from 'next/link';
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const PostLink = ( props ) => (
-	<li>
-		<Link as={`/p/${props.id}`} href={ `/post?title=${ props.title }` }>
-			<a>{ props.title }</a>
-		</Link>
-	</li>
-);
-
-const Index = () => (
+const Index = (props) => (
 	<Layout>
-		<h1>My Blog</h1>
+		<h1>Batman TV Shows</h1>
 		<ul>
-			<PostLink id="hello-nextjs" title="Hello Next.js" />
-			<PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-			<PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+			{props.shows.map(show => (
+				<li key={show.id}>
+					<Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+						<a>{show.name}</a>
+					</Link>
+				</li>
+			))}
 		</ul>
+		<style jsx>{`
+			h1, a {
+			font-family: "Arial";
+			color: 'red'
+		}
+
+			ul {
+			padding: 0;
+		}
+
+			li {
+			list-style: none;
+			margin: 5px 0;
+		}
+
+			a {
+			text-decoration: none;
+			color: blue;
+		}
+
+			a:hover {
+			opacity: 0.6;
+		}
+		`}
+		</style>
 	</Layout>
 );
 
-export default Index;
+Index.getInitialProps = async function() {
+	const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+	const data = await res.json();
+
+	console.log(`Show data fetched. Count: ${data.length}`);
+
+	return {
+		shows: data.map(entry => entry.show)
+	}
+};
+
+
+
+export default Index
