309a7e2820c6c572ba648c9e552aca767c24a112
Imran Sayed <imranhsayed@gmail.com>
2019-05-30 22:54:24 +0530

    Add isomorphic fetch

---
+++ pages/About.js
@@ -0,0 +1,5 @@
+const About = () => {
+
+};
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,38 @@
-export default () => (
-	<h1>Hello</h1>
-)
+import React from 'react';
+import './Style.css';
+import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
+
+export default class Index extends React.Component {
+
+	// We use fetch from isomorphic-fetch inside our async function getInitialProps()
+	// and what is return by this function will be available as props to your Index Page.
+	static async getInitialProps() {
+		const res = await fetch( 'https://api.tvmaze.com/search/shows?q=batman' );
+		const data = await res.json();
+
+		return {
+			shows: data
+		};
+	}
+
+	render() {
+		// This will contain data
+		console.warn( 'cards', this.props.shows );
+
+		return(
+			<div>
+				{/*Navigation*/}
+				<Link href="/"><a>Home</a></Link>
+				<Link href="/about"><a>About</a></Link>
+
+				{/*Heading*/}
+				<h1>React with Next JS</h1>
+
+				{/*Image*/}
+				<img src="/static/logo.png" width="200" alt="Logo"/>
+			</div>
+		)
+
+	}
+}
